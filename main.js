cbac0cd61704929e7063aa7a8352fcfae2f9c9a6
LNorza <luiscnorza@gmail.com>
2023-11-20 01:57:15 -0600

    Add Shopping Cart

--- main.js
+++ main.js
@@ -4,20 +4,44 @@ const desktopMenu = document.querySelector(".desktop-menu");
 /* Mobile Menu JS variables*/
 const menuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+/* Car-shop */
+const menuCar = document.querySelector(".navbar-shopping-cart");
+const asideCar = document.querySelector(".product-detail");
 
 navEmail.addEventListener("click", toggleDesktopMenu);
 menuIcon.addEventListener("click", toggleMobileMenu);
+menuCar.addEventListener("click", toggleAsideCar);
 
+/* function to activate the animation of desktop menu */
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+
+    if (!asideCar.classList.contains("hide-right")) {
+        asideCar.classList.toggle("hide-right");
+    }
 }
 
 /* function to activate the animation of mobile menu */
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("oculto");
-}
+    mobileMenu.classList.toggle("hide-left");
 
+    if (!asideCar.classList.contains("hide-right")) {
+        asideCar.classList.toggle("hide-right");
+    }
+}
 /* function to hide the mobile menu
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
 } */
+
+/* function to activate the animation of car-shop */
+function toggleAsideCar() {
+    asideCar.classList.toggle("hide-right");
+
+    if (!mobileMenu.classList.contains("hide-left")) {
+        mobileMenu.classList.toggle("hide-left");
+    }
+    if (!desktopMenu.classList.contains("inactive")) {
+        desktopMenu.classList.toggle("inactive");
+    }
+}
